8f311a00ce5044c688586a51ec04da1790bc2e50
husson61050280 <61050280@kmitl.ac.th>
2020-11-23 21:53:22 +0700

    g

--- controller/UserController.js
+++ controller/UserController.js
@@ -215,11 +215,11 @@ class UserController {
   ForgetPassword(req, res) {
     let email = req.body.email;
     User.CheckEmail(email, function (err, success) {
-      if (err) {
+      console.log("User", success);
+      if (success == 0) {
         let errors = "Email Not Found. Please Try again"
         res.render("/User/forgotPassword" , {errors : errors});
       }
-      console.log("User", success);
       if (success) {
         User.getUserByEmail(email, function (err, result) {
           let secret_token = result[0].secret_token;
